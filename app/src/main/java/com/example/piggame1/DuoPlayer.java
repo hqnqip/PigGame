d9d7a8f4fdcdd71fe8ccedde32d6be6c0e372b2f
DaleB34 <dalebolido@gmail.com>
2025-04-30 11:13:49 -0700

    added dice imageViews

--- app/src/main/java/com/example/piggame1/DuoPlayer.java
+++ app/src/main/java/com/example/piggame1/DuoPlayer.java
@@ -21,6 +21,7 @@ public class DuoPlayer extends AppCompatActivity {
     TextView slideBar;
     boolean isLeft;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
